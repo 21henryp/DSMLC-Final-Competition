f8f2e5f84b40c8f836f20873ec914ccc513c2550
Henry Pham <phung.pham@ucalgary.ca>
2024-03-08 14:18:02 -0700

    figuring out how to access cells' values

--- Cleaning.py
+++ Cleaning.py
@@ -1,6 +1,19 @@
 import pandas as pd
+from openpyxl import load_workbook as lwb
 
-dataset = "D:\School\Winter 2024\DSMLC Competition\DSMLC Final Competition 2024 Dataset.xlsx"
+# -------------------------------------------------------------
+def dropStyles(df: pd.DataFrame, workbook):
+  for i in range(len(df.index)):
+    for j in range(len(df.columns)):
+      cell = workbook.cell(i+1,j+1) #workbook not filtered 
+  
+  # print(cell.value)
+  # print(cell.row)
+  # print(df.iloc[cell.row - 1])
+
+# -------------------------------------------------------------
+dataset = ".\DSMLC Final Competition 2024 Dataset.xlsx"
+workbook = lwb(dataset, data_only=True).active
 df = pd.read_excel(dataset)
 
 # drop dulicates
@@ -8,5 +21,7 @@ df = df.drop_duplicates()
 # dataframe with countries only
 country_df = df.dropna(subset="Code")
 
+dropStyles(country_df, workbook)
+# print(country_df.iloc[len(country_df.index) - 1])
 
-print(country_df)
+# print(country_df.duplicated(subset="Country").value_counts())
