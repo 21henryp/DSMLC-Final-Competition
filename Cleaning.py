f1e3c97d950af64b1603af350a35eb0903542139
21henryp <115191366+21henryp@users.noreply.github.com>
2024-03-18 23:56:19 -0600

    Sorted out countries-only data

--- Cleaning.py
+++ Cleaning.py
@@ -1,27 +1,28 @@
 import pandas as pd
-from openpyxl import load_workbook as lwb
 
 # -------------------------------------------------------------
-def dropStyles(df: pd.DataFrame, workbook):
-  for i in range(len(df.index)):
-    for j in range(len(df.columns)):
-      cell = workbook.cell(i+1,j+1) #workbook not filtered 
-  
-  # print(cell.value)
-  # print(cell.row)
-  # print(df.iloc[cell.row - 1])
+
 
 # -------------------------------------------------------------
-dataset = ".\DSMLC Final Competition 2024 Dataset.xlsx"
-workbook = lwb(dataset, data_only=True).active
+dataset = "./DSMLC Final Competition 2024 Dataset.xlsx"
 df = pd.read_excel(dataset)
 
 # drop dulicates
 df = df.drop_duplicates()
-# dataframe with countries only
-country_df = df.dropna(subset="Code")
 
-dropStyles(country_df, workbook)
-# print(country_df.iloc[len(country_df.index) - 1])
+# convert file type to csv
+dataset = "./dataset.csv"
+df.to_csv(dataset)
+df = pd.read_csv(dataset)
 
+# dataframe with countries (on its own) only
+df = df.dropna(subset="Code")
+df = df.drop(df.columns[0], axis=1)
+# get rid of "() along with its annotations"
+df["Country"] = df["Country"].replace(regex=r'(.+?)\s\(.*\)', value=r'\1')
+df.to_csv(dataset)
+
+# --------------------------------------- #
+pd.set_option('display.max_rows', 4500)
+print(df)
 # print(country_df.duplicated(subset="Country").value_counts())
