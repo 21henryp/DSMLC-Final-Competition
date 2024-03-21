530f7219d17bd790eebd4c52bb6c1bb9353c9c6f
21henryp <115191366+21henryp@users.noreply.github.com>
2024-03-21 04:40:42 -0600

    Merged Cleaning_Data branch

--- Analysis.py
+++ Analysis.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 # Subsets creation
 dataset = "./dataset.csv"
 df = pd.read_csv(dataset)
-df = df.drop(df.columns[0], axis=1)
 
 # Data organized by Year
 df_by_years = {}
@@ -15,13 +14,15 @@ for i in range(2000,2020):
 
 # Compute average of all countries by each year for every category
 x = 0 # Categories
-mean_categories_by_years = [[0 for z in range(20)] for z in range(21)]
+mean_categories_by_years = [[0 for z in range(20)] for z in range(2)]
 z = 0
 for i in df.columns:
   if (z < 3):
     z += 1
     continue
   x += 1
+  if (x == 3):
+    break
   y = 0 # Years
   for j in range(2000,2020):
     y += 1
@@ -31,11 +32,14 @@ years = [year for year in range(2000,2020)]
 # for i in range(x):
 #   print(sc.linregress(years, mean_categories_by_years[i]))
 
+print(mean_categories_by_years)
+
 # print(mean_categories_by_years[0])
 # print(sc.linregress(years, mean_categories_by_years[0]))
-plt.scatter(years,mean_categories_by_years[0])
-plt.xticks(years)
-plt.xlabel("Year")
-plt.ylabel(str(df.columns[3]))
-plt.grid(visible=True)
-plt.show()
+
+# plt.scatter(years,mean_categories_by_years[0])
+# plt.xticks(years)
+# plt.xlabel("Year")
+# plt.ylabel(str(df.columns[3]))
+# plt.grid(visible=True)
+# plt.show()
